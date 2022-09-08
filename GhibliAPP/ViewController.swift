04a1936da082a51bf1362fe6d657e1df3ef28d17
Stephane <stephanegiraolinhares@gmail.com>
2022-09-08 09:47:50 -0300

    commit inicial

--- GhibliAPP/ViewController.swift
+++ GhibliAPP/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
