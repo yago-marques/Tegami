c1f9c278b26d434125c62f2fbc8251aefdce2d88
Stephane <stephanegiraolinhares@gmail.com>
2022-09-08 09:20:52 -0300

    Initial Commit

---
+++ GhibliAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhibliAPP
+//
+//  Created by Stephane Girão Linhares on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GhibliAPPUITests/GhibliAPPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GhibliAPPUITestsLaunchTests.swift
+//  GhibliAPPUITests
+//
+//  Created by Stephane Girão Linhares on 08/09/22.
+//
+
+import XCTest
+
+class GhibliAPPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
