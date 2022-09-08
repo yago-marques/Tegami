f90e2a6562b81fcb638184972b804c63b9472073
Yago Marques <yagovictormarques@gmail.com>
2022-09-08 11:32:29 -0300

    add and configure swiftlint

--- GhibliAPP/Application/SceneDelegate.swift
+++ GhibliAPP/Application/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -20,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let safeWindow = UIWindow(windowScene: windowScene)
         safeWindow.frame = UIScreen.main.bounds
         safeWindow.makeKeyAndVisible()
-        safeWindow.rootViewController = ViewController()
+        safeWindow.rootViewController = UINavigationController(rootViewController: OnbordingViewController())
+        
         self.window = safeWindow
     }
 
@@ -52,6 +52,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
---
+++ GhibliAPP/Source/Onboarding/View/OnbordingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OnbordingViewController.swift
+//  GhibliAPP
+//
+//  Created by Yago Marques on 08/09/22.
+//
+
+import UIKit
+
+final class OnbordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+
+}
