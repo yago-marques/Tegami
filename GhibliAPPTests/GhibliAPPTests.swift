2ddb43404cc91b4c1e93cabe98cfc989fb9fa6a9
Caio Soares <caiofasoares@gmail.com>
2022-09-14 11:43:27 -0300

    testing the mainScreenVIewModel async calls

--- GhibliAPPTests/GhibliAPPTests.swift
+++ GhibliAPPTests/GhibliAPPTests.swift
@@ -5,32 +5,32 @@
 //  Created by Stephane Girão Linhares on 08/09/22.
 //
 
+//estrutura base:
+//given
+//when
+//then
+
 import XCTest
 @testable import GhibliAPP
 
-class GhibliAPPTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+class MainScreenViewModelTests: XCTestCase {
+    
+    //viewModel que será usado em todos os tests
+    let viewModel = MainScreenViewModel(apiService: APICall())
+    
+    func test_transformGenresWithLongList_ArrayString() async {
+        //given
+        //precisamos dos IDs.
+        
+        let givenIds: [Int] = [28, 12, 13, 14, 15] // 28 é ação e 12 e aventura
+        
+        //when
+        let whenTransformGenresResult = await viewModel.transformGenres(ids: givenIds) ?? ["",""]
+        
+        //then
+        XCTAssertEqual(whenTransformGenresResult[0], "Ação")
+        XCTAssertEqual(whenTransformGenresResult[1], "Aventura")
+        
     }
 
 }
