2bde73962833686f9ba8e40d58dcdc9e7976116f
Caio Soares <caiofasoares@gmail.com>
2022-09-15 10:21:04 -0300

    Setting up the modelTest folder

---
+++ GhibliAPPTests/Model/FilmModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  FilmModel.swift
+//  GhibliAPPTests
+//
+//  Created by Caio Soares on 15/09/22.
+//
+
+import XCTest
+@testable import GhibliAPP
+
+class FilmModelTests: XCTestCase {
+    
+    
+    
+}
