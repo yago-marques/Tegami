450785d4e6808914ff1463c76f5e7770cd646ee4
Caio Soares <caiofasoares@gmail.com>
2022-09-21 09:09:34 -0300

    ...

--- GhibliAPP/Application/AppDelegate.swift
+++ GhibliAPP/Application/AppDelegate.swift
@@ -6,13 +6,11 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        IQKeyboardManager.shared.enable = true
           return true
         }
 
