12c27a3f7e054d109923fec0387480fa31c2761a
Yago Marques <yagovictormarques@gmail.com>
2022-09-28 07:53:51 -0300

    Initial Commit

---
+++ Tegami/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tegami
+//
+//  Created by Yago Marques on 28/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
