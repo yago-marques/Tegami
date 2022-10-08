ac8f673e9e980aca5f46a4e53ba15289c5d09902
Mateus Sales <matheussalles40@gmail.com>
2022-10-07 23:36:17 -0300

    Refactor `OnboardingViewController` to improve readability

--- Tegami/Source/Onboarding/ViewModel/OnboardingViewModel.swift
+++ Tegami/Source/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -8,42 +8,48 @@
 import UIKit
 
 protocol OnboardingViewModeling {
-    func markOnboardAsWatched()
     func getModel(at index: Int) -> OnboardingModel
     func numberOfRows() -> Int
     func presentOnboardingIfNeeded()
+    func setupTitle(at index: Int)
+    func navigateToHomeIfNeeded(buttonTitle: String?)
 }
 
 protocol OnboardginViewModelDelegate: AnyObject {
     func showOnboarding()
     func showMainScreen()
+    func setup(buttonTitle: String)
+    func displayScreen(at index: Int)
 }
 
 final class OnboardingViewModel: OnboardingViewModeling {
-    private let defaults: UserDefaultsProtocol
-    private let models: [OnboardingModel] = [
-        .init(
-            title: "Explore o vasto universo das animações do Studio Ghibli",
-            description: ""
-        ),
-        .init(
-            title: "Barra de XP",
-            description: "Maratone os filmes do Studio Ghibli e faça sua barra de XP crescer!! não esqueça de compartilhar sua evolução com os amigos"
-        ),
-        .init(
-            title: "Sua lista de filmes",
-            description: "Procuramos os cinco filmes mais populares do Studio Ghibli e adicionamos à sua lista de filmes :)"
-        )
-    ]
+    private enum ButtonType: String {
+        case next = "Próximo"
+        case start = "Quero começar"
+    }
+    
+    var models: [OnboardingModel] {
+        return [
+            .init(
+                title: "Explore o vasto universo das animações do Studio Ghibli",
+                description: ""
+            ),
+            .init(
+                title: "Barra de XP",
+                description: "Maratone os filmes do Studio Ghibli e faça sua barra de XP crescer!! não esqueça de compartilhar sua evolução com os amigos"
+            ),
+            .init(
+                title: "Sua lista de filmes",
+                description: "Procuramos os cinco filmes mais populares do Studio Ghibli e adicionamos à sua lista de filmes :)"
+            )
+        ]
+    }
 
+    private let defaults: UserDefaultsProtocol
     weak var delegate: OnboardginViewModelDelegate?
 
     init(defaults: UserDefaultsProtocol) {
         self.defaults = defaults
-    }
-
-    func markOnboardAsWatched() {
-        defaults.setBool(true, forKey: "onboard")
     }
     
     func getModel(at index: Int) -> OnboardingModel {
@@ -57,5 +63,17 @@ final class OnboardingViewModel: OnboardingViewModeling {
     func presentOnboardingIfNeeded() {
         let onboardWasSeen = defaults.bool(forKey: "onboard")
         onboardWasSeen ? delegate?.showMainScreen() : delegate?.showOnboarding()
+    }
+    
+    func setupTitle(at index: Int) {
+        let type: ButtonType = index == 2 ? .start : .next
+        delegate?.setup(buttonTitle: type.rawValue)
+    }
+    
+    func navigateToHomeIfNeeded(buttonTitle: String?) {
+        if let buttonTitle, buttonTitle == ButtonType.start.rawValue {
+            delegate?.showMainScreen()
+            defaults.setBool(true, forKey: "onboard")
+        }
     }
 }
