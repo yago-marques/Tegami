2f5913e1e1ce9b8d202d64198e53189eecb34b0f
Mateus Sales <matheussalles40@gmail.com>
2022-10-13 22:15:21 -0300

    Add correct control access to methods from RemoteFilmLoader

--- Tegami/Source/API/RemoteFilmLoader.swift
+++ Tegami/Source/API/RemoteFilmLoader.swift
@@ -22,8 +22,74 @@ final class RemoteFilmLoader: FilmLoader {
     init(api: HTTPClient) {
         self.api = api
     }
+        
+    func getFilms(completion: @escaping (Result<[Film], Error>) -> Void) {
+        fetchGhibliInfos { [weak self] in
+            guard let self = self else { return }
+            self.ghibliInfos.forEach {
+                self.fetchTmdbInfo(for: $0.originalTitle) {
+                    completion(self.makeFilms())
+                }
+            }
+        }
+    }
     
-    func makeFilms() -> Result<[Film], Error> {
+    // MARK: - Private Methods
+    
+    private func fetchGhibliInfos(completion: @escaping () -> Void) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        api.get(endpoint: GhibliFilmsEndpoint()) { [weak self] result in
+            guard let self = self else { return }
+            
+            if case let .success((data, response)) = result {
+                guard response.statusCode == 200, let infos = try? decoder.decode([GhibliInfo].self, from: data) else { return }
+                self.ghibliInfos = infos
+            }
+            
+            completion()
+        }
+    }
+    
+    private func fetchTmdbInfo(for title: String, completion: @escaping () -> Void) {
+        let endpoint = TmdbInfoEndpoint(title: title)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        api.get(endpoint: endpoint) { [weak self] result in
+            guard let self = self else { return }
+            
+            if case let .success((data, response)) = result {
+                guard response.statusCode == 200, let tmdbInfo = try? decoder.decode(TmdbInfo.self, from: data) else { return }
+                self.tmdbResults.append(tmdbInfo.results[0])
+            }
+            
+            if self.tmdbResults.count == self.ghibliInfos.count {
+                self.fetchGenres(completion: completion)
+            }
+            
+        }
+    }
+    
+    private func fetchGenres(completion: @escaping () -> Void) {
+        let endpoint = TmdbGenreEndpoint()
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        api.get(endpoint: endpoint) { [weak self] result in
+            guard let self = self else { return }
+            
+            if case let .success((data, response)) = result {
+                guard response.statusCode == 200, let model = try? decoder.decode(GenreModel.self, from: data) else { return }
+                self.genres = model.genres
+            }
+            
+            completion()
+        }
+    }
+    
+    private func makeFilms() -> Result<[Film], Error> {
         if ghibliInfos.isEmpty {
             return .failure(DomainError.invalidData)
         }
@@ -46,71 +112,5 @@ final class RemoteFilmLoader: FilmLoader {
         }
         
         return .success(films)
-    }
-    
-    func getFilms(completion: @escaping (Result<[Film], Error>) -> Void) {
-        fetchGhibliInfos { [weak self] in
-            guard let self = self else { return }
-            self.ghibliInfos.forEach {
-                self.fetchTmdbInfo(for: $0.originalTitle) {
-                    completion(self.makeFilms())
-                }
-            }
-        }
-    }
-    
-    // MARK: - Private Methods
-    
-    func fetchGhibliInfos(completion: @escaping () -> Void) {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        api.get(endpoint: GhibliFilmsEndpoint()) { [weak self] result in
-            guard let self = self else { return }
-            
-            if case let .success((data, response)) = result {
-                guard response.statusCode == 200, let infos = try? decoder.decode([GhibliInfo].self, from: data) else { return }
-                self.ghibliInfos = infos
-            }
-            
-            completion()
-        }
-    }
-    
-    func fetchTmdbInfo(for title: String, completion: @escaping () -> Void) {
-        let endpoint = TmdbInfoEndpoint(title: title)
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        api.get(endpoint: endpoint) { [weak self] result in
-            guard let self = self else { return }
-            
-            if case let .success((data, response)) = result {
-                guard response.statusCode == 200, let tmdbInfo = try? decoder.decode(TmdbInfo.self, from: data) else { return }
-                self.tmdbResults.append(tmdbInfo.results[0])
-            }
-            
-            if self.tmdbResults.count == self.ghibliInfos.count {
-                self.fetchGenres(completion: completion)
-            }
-            
-        }
-    }
-    
-    func fetchGenres(completion: @escaping () -> Void) {
-        let endpoint = TmdbGenreEndpoint()
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        api.get(endpoint: endpoint) { [weak self] result in
-            guard let self = self else { return }
-            
-            if case let .success((data, response)) = result {
-                guard response.statusCode == 200, let model = try? decoder.decode(GenreModel.self, from: data) else { return }
-                self.genres = model.genres
-            }
-            
-            completion()
-        }
     }
 }
